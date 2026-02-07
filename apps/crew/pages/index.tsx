73864e519d40a00c0c70b3f855db79e6e280b720
Marcos Grappeggia <marcosgrappeggia@gmail.com>
2026-02-07 15:28:44 -0800

    Move benchmark marker to leaf route app/bench/ for better incremental cache

--- apps/crew/pages/index.tsx
+++ apps/crew/pages/index.tsx
@@ -1,10 +1,8 @@
 import styles from './index.module.css';
-import { BenchmarkMarker } from './benchmark-marker';
 
 export function Index() {
   return (
     <div className={styles.page}>
-      <BenchmarkMarker />
       <h2>Index Page</h2>
     </div>
   );
